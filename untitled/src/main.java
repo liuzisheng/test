9aaf44d098a4022c0fb3ec89513f825189f9d630
lzs <lzs187@163.com>
2020-02-25 17:54:12 +0800

    忽略之前的

---
+++ untitled/src/main.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName main
+ * @Description TODO
+ * @Author liuzisheng
+ * @Date 2020/2/25 17:52
+ * @Version 1.0
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
